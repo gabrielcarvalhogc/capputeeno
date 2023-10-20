954b42812d840af33541870f859aac5767af1d77
Gabriel Carvalho <gabriel225gc@gmail.com>
2023-10-20 16:17:52 -0300

    refactor: extract providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Saira } from 'next/font/google'
 import './globals.css'
 import Header from '../components/header/header'
-import { FilterContextProvider } from '@/contexts/filter-context'
+import { DefaultProviders } from '@/components/default-providers'
 
 const saira = Saira({
   subsets: ['latin'],
@@ -22,10 +22,10 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={saira.className}>
-        <FilterContextProvider>
+        <DefaultProviders>
           <Header />
           {children}
-        </FilterContextProvider>
+        </DefaultProviders>
       </body>
     </html>
   )
